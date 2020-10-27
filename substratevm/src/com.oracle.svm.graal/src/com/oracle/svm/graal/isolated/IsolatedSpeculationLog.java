bbf7b3086053507d92e4b620e97e669c34852a15
Foivos Zakkak <fzakkak@redhat.com>
2020-10-27 23:04:31 +0200

    Work around the absence of SpeculationReasonEncoding in OpenJDK11

--- substratevm/src/com.oracle.svm.graal/src/com/oracle/svm/graal/isolated/IsolatedSpeculationLog.java
+++ substratevm/src/com.oracle.svm.graal/src/com/oracle/svm/graal/isolated/IsolatedSpeculationLog.java
@@ -24,8 +24,18 @@
  */
 package com.oracle.svm.graal.isolated;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.nio.ByteBuffer;
+import java.util.function.Supplier;
 
+import jdk.vm.ci.meta.ResolvedJavaField;
+import jdk.vm.ci.meta.ResolvedJavaMethod;
+import jdk.vm.ci.meta.ResolvedJavaType;
+import org.graalvm.compiler.serviceprovider.UnencodedSpeculationReason;
 import org.graalvm.nativeimage.PinnedObject;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
 import org.graalvm.nativeimage.c.type.CTypeConversion;
@@ -48,10 +58,130 @@ public final class IsolatedSpeculationLog extends IsolatedObjectProxy<Speculatio
         collectFailedSpeculations0(IsolatedCompileContext.get().getClient(), handle);
     }
 
+    static class SpeculationReasonInvocationHandler implements InvocationHandler {
+        private final IsolatedSpeculationReasonEncoding receiver;
+
+        SpeculationReasonInvocationHandler(IsolatedSpeculationReasonEncoding receiver) {
+            this.receiver = receiver;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            return IsolatedSpeculationReasonEncoding.class.getDeclaredMethod(method.getName(), method.getParameterTypes()).invoke(receiver, args);
+        }
+    }
+
+    static class SpeculationReasonEncodingSupplier implements Supplier<Object> {
+        private static final Class<?> PROXY_CLASS;
+        private static final Constructor<?> CONSTRUCTOR;
+        private static final Class<?> CLASS;
+        private static final String CLASS_NAME = "jdk.vm.ci.meta.SpeculationLog$SpeculationReasonEncoding";
+
+        static {
+            Class<?> speculationReasonEncodingClass = null;
+            try {
+                speculationReasonEncodingClass = Class.forName(CLASS_NAME);
+            } catch (ClassNotFoundException e) {
+                VMError.shouldNotReachHere(CLASS_NAME +
+                                " not found in JDK. UnencodedSpeculationReason should have been used as SpeculationReason");
+            }
+            CLASS = speculationReasonEncodingClass;
+            assert CLASS != null;
+            PROXY_CLASS = Proxy.newProxyInstance(CLASS.getClassLoader(), new Class<?>[]{CLASS},
+                            new SpeculationReasonInvocationHandler(new IsolatedSpeculationReasonEncoding())).getClass();
+            Constructor<?> declaredConstructor = null;
+            try {
+                declaredConstructor = PROXY_CLASS.getDeclaredConstructor(CLASS);
+            } catch (NoSuchMethodException e) {
+                VMError.shouldNotReachHere("Failed to get constructor for " + PROXY_CLASS);
+            }
+            CONSTRUCTOR = declaredConstructor;
+        }
+
+        @Override
+        public Object get() {
+            try {
+                return CONSTRUCTOR.newInstance(new SpeculationReasonInvocationHandler(new IsolatedSpeculationReasonEncoding()));
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                VMError.shouldNotReachHere("Failed to instantiate " + CLASS_NAME + "through proxy", e);
+            }
+            return null;
+        }
+    }
+
+    private byte[] encodeAsByteArray(SpeculationReason reason) {
+        byte[] bytes = null;
+        if (reason instanceof UnencodedSpeculationReason) {
+            IsolatedSpeculationReasonEncoding encoding = encode((UnencodedSpeculationReason) reason);
+            bytes = encoding.getByteArray();
+        } else {
+            try {
+                final Method encode = Class.forName("jdk.vm.ci.meta.EncodedSpeculationReason")
+                                .getDeclaredMethod("encode", Supplier.class);
+                IsolatedSpeculationReasonEncoding encoding = (IsolatedSpeculationReasonEncoding) encode.invoke(reason, new SpeculationReasonEncodingSupplier());
+                bytes = encoding.getByteArray();
+            } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+                VMError.shouldNotReachHere("Failed to invoke \"jdk.vm.ci.meta.EncodedSpeculationReason#encode\" through reflection", e);
+            }
+        }
+        return bytes;
+    }
+
+    /** Adapted from {@code jdk.vm.ci.meta.EncodedSpeculationReason#encode}. */
+    public IsolatedSpeculationReasonEncoding encode(UnencodedSpeculationReason reason) {
+        IsolatedSpeculationReasonEncoding encoding = new IsolatedSpeculationReasonEncoding();
+        try {
+            int groupId = UnencodedSpeculationReason.class.getDeclaredField("groupId").getInt(reason);
+            Object[] context = (Object[]) UnencodedSpeculationReason.class.getDeclaredField("context").get(reason);
+            encoding.addInt(groupId);
+            for (Object o : context) {
+                if (o == null) {
+                    encoding.addInt(0);
+                } else {
+                    addNonNullObject(encoding, o);
+                }
+            }
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            VMError.shouldNotReachHere("Failed to encode speculation reason", e);
+        }
+        return encoding;
+    }
+
+    /** Copied from {@code jdk.vm.ci.meta.EncodedSpeculationReason#addNonNullObject}. */
+    static void addNonNullObject(IsolatedSpeculationReasonEncoding encoding, Object o) {
+        Class<?> c = o.getClass();
+        if (c == String.class) {
+            encoding.addString((String) o);
+        } else if (c == Byte.class) {
+            encoding.addByte((Byte) o);
+        } else if (c == Short.class) {
+            encoding.addShort((Short) o);
+        } else if (c == Character.class) {
+            encoding.addShort((Character) o);
+        } else if (c == Integer.class) {
+            encoding.addInt((Integer) o);
+        } else if (c == Long.class) {
+            encoding.addLong((Long) o);
+        } else if (c == Float.class) {
+            encoding.addInt(Float.floatToRawIntBits((Float) o));
+        } else if (c == Double.class) {
+            encoding.addLong(Double.doubleToRawLongBits((Double) o));
+        } else if (o instanceof Enum) {
+            encoding.addInt(((Enum<?>) o).ordinal());
+        } else if (o instanceof ResolvedJavaMethod) {
+            encoding.addMethod((ResolvedJavaMethod) o);
+        } else if (o instanceof ResolvedJavaType) {
+            encoding.addType((ResolvedJavaType) o);
+        } else if (o instanceof ResolvedJavaField) {
+            encoding.addField((ResolvedJavaField) o);
+        } else {
+            throw new IllegalArgumentException("Unsupported type for encoding: " + c.getName());
+        }
+    }
+
     @Override
     public boolean maySpeculate(SpeculationReason reason) {
-        SpeculationReasonEncoding encoding = reason.encode(IsolatedSpeculationReasonEncoding::new);
-        byte[] bytes = ((IsolatedSpeculationReasonEncoding) encoding).getByteArray();
+        byte[] bytes = encodeAsByteArray(reason);
         try (PinnedObject pinnedBytes = PinnedObject.create(bytes)) {
             return maySpeculate0(IsolatedCompileContext.get().getClient(), handle, pinnedBytes.addressOfArrayElement(0), bytes.length);
         }
@@ -59,8 +189,7 @@ public final class IsolatedSpeculationLog extends IsolatedObjectProxy<Speculatio
 
     @Override
     public Speculation speculate(SpeculationReason reason) {
-        SpeculationReasonEncoding encoding = reason.encode(IsolatedSpeculationReasonEncoding::new);
-        byte[] bytes = ((IsolatedSpeculationReasonEncoding) encoding).getByteArray();
+        byte[] bytes = encodeAsByteArray(reason);
         ClientHandle<SpeculationReason> reasonHandle;
         try (PinnedObject pinnedBytes = PinnedObject.create(bytes)) {
             reasonHandle = speculate0(IsolatedCompileContext.get().getClient(), handle, pinnedBytes.addressOfArrayElement(0), bytes.length);
