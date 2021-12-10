a650001152fcbb255a16cbb04f9f7476549e5e03
Foivos Zakkak <fzakkak@redhat.com>
2021-12-10 11:46:44 +0200

    CI: Substitute 999-SNAPSHOT with 2.2 in java files

--- .github/update_quarkus_version.sh
+++ .github/update_quarkus_version.sh
@@ -1,1 +1,1 @@
-find -type f \( -name "*.xml" -o -name "*.properties" \) -exec sed -i "s/999-SNAPSHOT/$1/g" {} +
+find -type f \( -name "*.xml" -o -name "*.properties" -o -name "*.java" \) -exec sed -i "s/999-SNAPSHOT/$1/g" {} +
