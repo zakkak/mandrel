ceb8e169f0dd5312bd389369d084c905d4677890
Severin Gehwolf <sgehwolf@redhat.com>
2022-10-11 19:22:32 +0200

    Set vm/mx.vm suite release to false

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -2,7 +2,7 @@ suite = {
     "name": "vm",
     "version" : "21.3.3.1",
     "mxversion" : "5.309.2",
-    "release" : True,
+    "release" : False,
     "groupId" : "org.graalvm",
 
     "url" : "http://www.graalvm.org/",
