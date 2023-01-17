7da419b9ac04495f8846e929bb15dbcea2d00b58
Foivos Zakkak <fzakkak@redhat.com>
2023-01-17 20:01:26 +0200

    Unmark suites and bump version to 21.3.5.1 [skip ci]

--- regex/mx.regex/suite.py
+++ regex/mx.regex/suite.py
@@ -43,8 +43,8 @@ suite = {
 
   "name" : "regex",
 
-  "version" : "21.3.5.0",
-  "release" : True,
+  "version" : "21.3.5.1",
+  "release" : False,
   "groupId" : "org.graalvm.regex",
   "url" : "http://www.graalvm.org/",
   "developer" : {
