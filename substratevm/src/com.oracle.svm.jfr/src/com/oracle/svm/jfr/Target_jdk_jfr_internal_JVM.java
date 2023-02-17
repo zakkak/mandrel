d1bf8d867e28de639b0395248c88b99ed93d5ed7
Foivos Zakkak <fzakkak@redhat.com>
2023-02-17 12:45:32 +0200

    Fix JDK-17 builds with jdk-17.0.7+1 and later
    FILE_DELTA_CHANGE got removed and CHUNK_ROTATION_MONITOR got introduced
    by https://bugs.openjdk.org/browse/JDK-8286707 starting with
    jdk-17.0.7+1
    
    (cherry picked from commit 2f9018e77a9f4c6c36127d0abdf4378269fb572d)

--- substratevm/src/com.oracle.svm.jfr/src/com/oracle/svm/jfr/Target_jdk_jfr_internal_JVM.java
+++ substratevm/src/com.oracle.svm.jfr/src/com/oracle/svm/jfr/Target_jdk_jfr_internal_JVM.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,6 +25,7 @@
 package com.oracle.svm.jfr;
 
 import java.util.List;
+import java.util.function.BooleanSupplier;
 
 import org.graalvm.nativeimage.ProcessProperties;
 
@@ -41,6 +42,7 @@ import com.oracle.svm.core.jdk.JDK15OrLater;
 import com.oracle.svm.core.jdk.JDK16OrEarlier;
 import com.oracle.svm.core.util.VMError;
 import com.oracle.svm.jfr.traceid.JfrTraceId;
+import com.oracle.svm.util.ReflectionUtil;
 
 import jdk.jfr.Event;
 import jdk.jfr.internal.EventWriter;
@@ -53,8 +55,38 @@ import jdk.jfr.internal.handlers.EventHandler;
 @TargetClass(value = jdk.jfr.internal.JVM.class, onlyWith = JfrEnabled.class)
 public final class Target_jdk_jfr_internal_JVM {
     // Checkstyle: stop
-    @Alias static Object FILE_DELTA_CHANGE;
+    @Alias @TargetElement(onlyWith = JvmChunkRotationMonitorAvailable.class) //
+    static Object CHUNK_ROTATION_MONITOR;
+
+    @Alias @TargetElement(onlyWith = JvmFileDeltaChangeAvailable.class) //
+    static Object FILE_DELTA_CHANGE;
     // Checkstyle: resume
+
+    static class JvmChunkRotationMonitorAvailable extends JvmFieldAvailable {
+        protected JvmChunkRotationMonitorAvailable() {
+            super("CHUNK_ROTATION_MONITOR");
+        }
+    }
+
+    private static class JvmFileDeltaChangeAvailable extends JvmFieldAvailable {
+        protected JvmFileDeltaChangeAvailable() {
+            super("FILE_DELTA_CHANGE");
+        }
+    }
+
+    private abstract static class JvmFieldAvailable implements BooleanSupplier {
+
+        private final String fieldName;
+
+        protected JvmFieldAvailable(String fieldName) {
+            this.fieldName = fieldName;
+        }
+
+        @Override
+        public boolean getAsBoolean() {
+            return ReflectionUtil.lookupField(true, JVM.class, fieldName) != null;
+        }
+    }
 
     @Alias @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset) //
     private volatile boolean nativeOK;
