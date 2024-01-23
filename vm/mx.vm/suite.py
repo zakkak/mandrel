af3ef2cc7e29e040df1b79114762202c8f679de9
Michal Karm Babacek <karm@fedoraproject.org>
2024-01-24 00:16:13 +0100

    Unmark suites and bump version to 23.1.2.1 [skip ci]

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -1,8 +1,8 @@
 suite = {
     "name": "vm",
-    "version" : "23.1.2.0",
+    "version" : "23.1.2.1",
     "mxversion": "6.41.0",
-    "release" : True,
+    "release" : False,
     "groupId" : "org.graalvm",
 
     "url" : "http://www.graalvm.org/",
