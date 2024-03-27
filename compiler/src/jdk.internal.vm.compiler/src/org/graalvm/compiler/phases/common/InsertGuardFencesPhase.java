oracle/graal

[GR-52324] Add InsertGuardFencesPhase into runtime compilation phases.

compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/phases/common/InsertGuardFencesPhase.java
@@ -32,6 +32,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import jdk.graal.compiler.core.common.SpectrePHTMitigations;
 import jdk.graal.compiler.core.common.type.IntegerStamp;
 import jdk.graal.compiler.debug.Assertions;
 import jdk.graal.compiler.debug.DebugContext;
@@ -68,6 +69,10 @@ public class InsertGuardFencesPhase extends Phase {
 
     @Override
     protected void run(StructuredGraph graph) {
+        SpectrePHTMitigations mitigations = SpectrePHTBarriers.getValue(graph.getOptions());
+        if (mitigations == SpectrePHTMitigations.None || mitigations == SpectrePHTMitigations.AllTargets) {
+            return;
+        }
         ControlFlowGraph cfg = ControlFlowGraph.newBuilder(graph).connectBlocks(true).computeFrequency(true).build();
         for (AbstractBeginNode beginNode : graph.getNodes(AbstractBeginNode.TYPE)) {
             if (hasPotentialUnsafeAccess(cfg, beginNode)) {
@@ -76,7 +81,7 @@ public class InsertGuardFencesPhase extends Phase {
                 continue;
             }
             if (hasGuardUsages(beginNode)) {
-                if (SpectrePHTBarriers.getValue(graph.getOptions()) == NonDeoptGuardTargets) {
+                if (mitigations == NonDeoptGuardTargets) {
                     if (isDeoptGuard(beginNode)) {
                         graph.getDebug().log(DebugContext.VERBOSE_LEVEL, "Skipping deoptimizing guard speculation fence at %s", beginNode);
                         continue;

substratevm/src/com.oracle.svm.truffle/src/com/oracle/svm/truffle/TruffleFeature.java
@@ -100,6 +100,7 @@ import java.util.function.ToLongBiFunction;
 import java.util.function.ToLongFunction;
 import java.util.function.UnaryOperator;
 
+import jdk.graal.compiler.phases.common.InsertGuardFencesPhase;
 import org.graalvm.collections.Pair;
 import org.graalvm.nativeimage.AnnotationAccess;
 import org.graalvm.nativeimage.ImageSingletons;
@@ -969,6 +970,15 @@ public class TruffleFeature implements InternalFeature {
          */
         if (hosted && HostInliningPhase.Options.TruffleHostInlining.getValue(HostedOptionValues.singleton()) && suites.getHighTier() instanceof HighTier) {
             suites.getHighTier().prependPhase(new SubstrateHostInliningPhase(CanonicalizerPhase.create()));
+        }
+        /*
+         * On HotSpot, the InsertGuardFencesPhase is inserted into the mid-tier depending on the
+         * runtime option SpectrePHTBarriers. However, TruffleFeature registers phases during
+         * image-build time. Therefore, for Truffle compilations, we need to register the phase
+         * eagerly because the SpectrePHTBarriers options is set only at image-execution time.
+         */
+        if (!hosted && suites.getMidTier().findPhase(InsertGuardFencesPhase.class, true) == null) {
+            suites.getMidTier().appendPhase(new InsertGuardFencesPhase());
         }
     }
 }

zakkak/mandrel

(cherry picked from commit 3c8e0e0d39f6ce5b0cd1458a8ac78d6897f82848)

compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/phases/common/InsertGuardFencesPhase.java
@@ -32,6 +32,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.graalvm.compiler.core.common.SpectrePHTMitigations;
 import org.graalvm.compiler.core.common.type.IntegerStamp;
 import org.graalvm.compiler.debug.DebugContext;
 import org.graalvm.compiler.graph.Node;
@@ -68,6 +69,10 @@ public class InsertGuardFencesPhase extends Phase {
 
     @Override
     protected void run(StructuredGraph graph) {
+        SpectrePHTMitigations mitigations = SpectrePHTBarriers.getValue(graph.getOptions());
+        if (mitigations == SpectrePHTMitigations.None || mitigations == SpectrePHTMitigations.AllTargets) {
+            return;
+        }
         ControlFlowGraph cfg = ControlFlowGraph.compute(graph, true, false, false, false);
         for (AbstractBeginNode beginNode : graph.getNodes(AbstractBeginNode.TYPE)) {
             if (hasPotentialUnsafeAccess(cfg, beginNode)) {
@@ -76,7 +81,7 @@ public class InsertGuardFencesPhase extends Phase {
                 continue;
             }
             if (hasGuardUsages(beginNode)) {
-                if (SpectrePHTBarriers.getValue(graph.getOptions()) == NonDeoptGuardTargets) {
+                if (mitigations == NonDeoptGuardTargets) {
                     if (isDeoptGuard(beginNode)) {
                         graph.getDebug().log(DebugContext.VERBOSE_LEVEL, "Skipping deoptimizing guard speculation fence at %s", beginNode);
                         continue;

substratevm/src/com.oracle.svm.truffle/src/com/oracle/svm/truffle/TruffleFeature.java
@@ -103,6 +103,7 @@ import java.util.function.ToLongBiFunction;
 import java.util.function.ToLongFunction;
 import java.util.function.UnaryOperator;
 
+import org.graalvm.compiler.phases.common.InsertGuardFencesPhase;
 import org.graalvm.collections.Pair;
 import org.graalvm.compiler.api.replacements.SnippetReflectionProvider;
 import org.graalvm.compiler.core.phases.HighTier;
@@ -996,6 +997,15 @@ public class TruffleFeature implements InternalFeature {
          */
         if (hosted && HostInliningPhase.Options.TruffleHostInlining.getValue(HostedOptionValues.singleton()) && suites.getHighTier() instanceof HighTier) {
             suites.getHighTier().prependPhase(new SubstrateHostInliningPhase(CanonicalizerPhase.create()));
+        }
+        /*
+         * On HotSpot, the InsertGuardFencesPhase is inserted into the mid-tier depending on the
+         * runtime option SpectrePHTBarriers. However, TruffleFeature registers phases during
+         * image-build time. Therefore, for Truffle compilations, we need to register the phase
+         * eagerly because the SpectrePHTBarriers options is set only at image-execution time.
+         */
+        if (!hosted && suites.getMidTier().findPhase(InsertGuardFencesPhase.class, true) == null) {
+            suites.getMidTier().appendPhase(new InsertGuardFencesPhase());
         }
     }
 }