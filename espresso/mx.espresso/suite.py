oracle/graal

Upgrade ASM dependency from 9.5 to 9.7.1.

espresso/mx.espresso/suite.py
@@ -346,7 +346,7 @@ suite = {
             "javaCompliance" : "17+",
             "spotbugs" : "false",
             "shadedDependencies" : [
-                "truffle:ASM_9.5",
+                "truffle:ASM_9.7.1",
             ],
             "class" : "ShadedLibraryProject",
             "shade" : {

zakkak/mandrel

(cherry picked from commit a55ea605af4c0ba6209e1a5ab5f90ed9c7ff6981)

espresso/mx.espresso/suite.py
@@ -111,7 +111,7 @@ suite = {
             "dependencies": [
                 "truffle:TRUFFLE_API",
                 "truffle:TRUFFLE_NFI",
-                "truffle:TRUFFLE_ASM_9.5",
+                "truffle:TRUFFLE_ASM_9.7.1",
                 "com.oracle.truffle.espresso.jdwp",
             ],
             "requires": [